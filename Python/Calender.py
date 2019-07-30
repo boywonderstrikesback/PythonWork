3d0d2b63e2c2860ccdcb50f974271aa619b2a021
Mike Ross <34175985+boywonderstrikesback@users.noreply.github.com>
2019-07-30 12:40:30 -0400

    Add files via upload

---
+++ Python/Calender.py
@@ -0,0 +1,16 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: /Users/mikeross/Documents/Rice.py =================
+Enter year: 90
+Enter month: 08
+     August 90
+Mo Tu We Th Fr Sa Su
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28 29 30 31
+
+>>> 
